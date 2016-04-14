724e38a66aeeeeab38d3dac13cf22069a400b8e5
Keaton Burleson <keaton.burleson@me.com>
2016-04-13 21:51:37 -0500

    added error handling, retweet, and reply.

--- LPTwitterViewController.h
+++ LPTwitterViewController.h
@@ -1,10 +1,11 @@
 
 #import "lockpages/LPPage-Protocol.h"
 #import "MBProgressHUD.h"
-@interface LPTwitterViewController : UITableViewController <LPPage>
+@interface LPTwitterViewController : UITableViewController <LPPage, UITextFieldDelegate>
 @property (nonatomic, retain) UIView *ibView;
 @property (strong, nonatomic) NSArray *dataSource;
 @property (strong, nonatomic) MBProgressHUD *hud;
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
+@property (strong, nonatomic) NSString *username;
 @end
 
